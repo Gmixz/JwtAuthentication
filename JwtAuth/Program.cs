3bf92b97090477cf5a8babd2132872f42e0caaf6
Gmixz <ros.biomed@gmail.com>
2025-04-06 20:33:06 -0400

    Add Entity Framework Core and Database Context

--- JwtAuth/Program.cs
+++ JwtAuth/Program.cs
@@ -1,3 +1,5 @@
+using JwtAuth.Data;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi("v1");
+
+builder.Services.AddDbContext<UserDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("UserDataBase")));
 
 var app = builder.Build();
 
